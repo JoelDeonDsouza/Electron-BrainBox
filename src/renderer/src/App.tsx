32e6ffb2cf44d9e15990f7407608459181118e78
joeldeondsouza <jdeondsouza@gmail.com>
2024-05-07 15:33:30 +0200

    boilerplate code removed and incorporated tailwind css

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,8 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
-
-function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
+function App() {
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-blue-500">Hello world</span>
+    </div>
   )
 }
-
 export default App
